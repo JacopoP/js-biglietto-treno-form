1c94d0081a8bad2331850045575c51d5d3bf6bac
Jac22 <jacopo.prudentino@gmail.com>
2022-11-08 17:37:38 +0100

    Completato javascripte html senza stile

--- js/myscript.js
+++ js/myscript.js
@@ -1,32 +1,44 @@
-let nameSurname;
-
 let generalities=document.getElementById("name");
-
-let age;
 
 let yOld=document.getElementById("age");
 
-let distance;
-
 let km=document.getElementById("km");
+
+let price;
 
 const generate=document.getElementById("confirm");
 
 const cancel=document.getElementById("cancel");
 
+const ticket=document.getElementById("show-hide");
+
 generate.addEventListener("click", 
     function(){
-        nameSurname=generalities.value;
-        
-        distance=parseInt(km.value);
-        
-        age=yOld.value;        
+        ticket.className="show";
 
-        console.log(nameSurname, age, distance);
+        document.getElementById("name-surname").innerHTML=generalities.value;
 
-        generalities.value=null;
-        km.value=null;
-        yOld.value="maggiorenne"
+        price = 0.21 * parseInt(km.value);
+
+        if(yOld.value==="minorenne"){
+            document.getElementById("offer").innerHTML="Ridotto minore et&agrave;";
+            price= price * 0.8;
+        }
+        
+        else if(yOld.value==="65+"){
+            document.getElementById("offer").innerHTML="Ridotto over 65";
+            price = price * 0.6;
+        }
+
+        else{
+            document.getElementById("offer").innerHTML="Biglietto standard";
+        }
+
+        document.getElementById("carriage").innerHTML=Math.floor(Math.random() * 10 + 1);
+
+        document.getElementById("cp").innerHTML=Math.floor(Math.random() * 10000);
+
+        document.getElementById("price").innerHTML=price.toFixed(2);
     }
 )
 
@@ -35,25 +47,7 @@ cancel.addEventListener("click",
         generalities.value=null;
         km.value=null;
         yOld.value="maggiorenne"
+        price=null;
+        ticket.className="hide";
     }
 )
-
-
-
-// calcolare prezzo
-let price = 0.21 * distance;
-
-if(age==="minorenne"){
-    price= price * 0.8;
-}
-
-else if(age==="65+"){
-    price = price * 0.6;
-}
-
-price=price.toFixed(2);
-
-console.log(price);
-
-// Frase d'uscita
-document.getElementById("price").innerHTML=`Il tuo biglietto costa ${price}&euro;.`;
